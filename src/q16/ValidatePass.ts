7259525990d3ac8d0137345414a45e83273f71f0
Sa1i3rii <aomsinwarit@gmail.com>
2025-12-15 18:10:22 +0700

    q

--- src/q16/ValidatePass.ts
+++ src/q16/ValidatePass.ts
@@ -2,7 +2,7 @@ const password = process.argv[2];
 
 const least8 = password.length >= 8;
 const number = /\d/.test(password);
-const uppcder = /[A-Z]/.test(password);
+const upper = /[A-Z]/.test(password);
 
 if (least8 && number && upper) {
   console.log("Valid");
