11cde30bbcd8c98de27517d04349328e157156cd
Sa1i3rii <aomsinwarit@gmail.com>
2025-12-15 17:57:43 +0700

    quizz

--- src/q17/starPyramid.ts
+++ src/q17/starPyramid.ts
@@ -1,8 +1,8 @@
 const num = Number(process.argv[2]);
-for(let i=0; i<num; i++){
+for(let i=1; i<=num; i++){
   let str = "";
-  for(let j=0; j<=i; j++){
-    str += "* ";
+  for(let j=1; j<=i; j++){
+    str += "*";
   }
   console.log(str);
 }
